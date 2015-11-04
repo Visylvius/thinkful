70b7b78cd6e8f6dd69ca04194b19db369acffd95
Loren Barrick <lorenbarrick@gmail.com>
2015-11-04 15:40:39 -0800

    first problem

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function quiz() {
+ return function() {var answer1 = "blue",
+  answer2 = "no",
+  answer3 = "yes";
+  var userAnswer1 = prompt("what color is the sky?");
+  if (userAnswer1 === answer1) {
+    alert("Correct");
+  } else {
+      alert("Sorry, that was wrong");
+  }
+    var userAnswer2 = prompt("Do cows fly?");
+    if (userAnswer2 === answer2) {
+      alert("Correct");
+    } else {
+      alert("Sorry, that was wrong");
+    }
+    var userAnswer3 = prompt("Do birds fly?");
+    if (userAnswer3 === answer3) {
+      alert("Correct");
+    } else {
+      alert("Sorry, that was wrong");
+    }
+ };
+}
+var myQuiz = quiz();
+console.log(myQuiz());
